d8b913ef16f9202d6ee908a6318353ce43e9f686
Jean-Louis Leysens <jean-louis@animo.id>
2021-11-05 14:48:09 +0100

    added text area component

--- src/components/index.ts
+++ src/components/index.ts
@@ -6,6 +6,8 @@ export * from './text';
 
 export * from './text-input';
 
+export * from './text-area';
+
 export * from './dropdown';
 
 export * from './combo-box';
--- src/components/text-area.tsx
+++ src/components/text-area.tsx
@@ -1,23 +1,25 @@
 import * as React from 'react';
 
-import { Input } from '@chakra-ui/react';
+import { Textarea } from '@chakra-ui/react';
 
 import { FormField } from './form-field';
 
 interface Props {
   label: string;
   value: string;
+  error?: string;
   onChange: (ev: any) => void;
 }
 
-export const TextInput: React.FunctionComponent<Props> = ({
+export const TextArea: React.FunctionComponent<Props> = ({
   label,
   value,
   onChange,
+  error,
 }) => {
   return (
-    <FormField label={label}>
-      <Input
+    <FormField label={label} error={error}>
+      <Textarea
         value={value}
         onChange={(e) => {
           onChange(e.target.value);
