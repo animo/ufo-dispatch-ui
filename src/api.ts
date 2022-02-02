0cd242a7fb202d56ff3e3aa0e7e44103ad0f38c5
Karim Stekelenburg <karim@animo.id>
2022-02-02 15:31:30 +0100

    docs: update readme
    Signed-off-by: Karim Stekelenburg <karim@animo.id>

--- src/api.ts
+++ src/api.ts
@@ -1,20 +1,27 @@
-import axios from 'axios'
-import { AddEmergencyPostBody, EmergencyResponse, EmergencyType, Qualification } from './types'
+import axios from "axios";
+import {
+  AddEmergencyPostBody,
+  EmergencyResponse,
+  EmergencyType,
+  Qualification,
+} from "./types";
 
-const endpoint = 'https://debug.dispatch.ufo.animo.id'
+const endpoint = "http://127.0.0.1:8080";
 
-const get = <R>(path: string): Promise<R> => axios.get(`${endpoint}/${path}`).then((res) => res.data)
+const get = <R>(path: string): Promise<R> =>
+  axios.get(`${endpoint}/${path}`).then((res) => res.data);
 const post = <R>(path: string, body: Record<string, unknown>): Promise<R> =>
-  axios.post(`${endpoint}/${path}`, body).then((res) => res.data)
+  axios.post(`${endpoint}/${path}`, body).then((res) => res.data);
 
 const api = {
-  emergencyType: () => get<EmergencyType[]>('emergencyType'),
-  qualifications: () => get<Qualification[]>('qualification'),
-  emergency: () => get('emergency'),
+  emergencyType: () => get<EmergencyType[]>("emergencyType"),
+  qualifications: () => get<Qualification[]>("qualification"),
+  emergency: () => get("emergency"),
   emergencyById: (id: number) => get<EmergencyResponse>(`emergency/${id}`),
   post: {
-    emergency: (body: AddEmergencyPostBody) => post<EmergencyResponse>('emergency', body),
+    emergency: (body: AddEmergencyPostBody) =>
+      post<EmergencyResponse>("emergency", body),
   },
-}
+};
 
-export { api }
+export { api };
